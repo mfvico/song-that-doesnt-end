61058aa913fa1a2d2765e2832058fbdbb103d24a
Mello <dan.mello1001@gmail.com>
2014-10-14 10:40:44 -0600

    Initial upload of exersize

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  root to: "pages#index"
+  root to: "pages#index", as: :root
+  get 'page-2' => 'pages#page2', as: :page_2
+  get 'page-3' => 'pages#page3', as: :page_3
+  get 'page-4' => 'pages#page4', as: :page_4
 end
